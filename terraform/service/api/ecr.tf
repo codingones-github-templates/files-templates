6f78b1c0c6060552f31cdc1515a36329490ee0c4
Romain Cambonie <romain.cambonie@gmail.com>
2023-04-20 09:41:13 +0200

    feat: add semantic release to ecr to fastify api files

--- terraform/service/api/ecr.tf
+++ terraform/service/api/ecr.tf
@@ -1,10 +1,19 @@
 resource "aws_ecr_repository" "api" {
   name                 = "api"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = false
   }
 
   tags = local.tags
+}
+
+data "aws_ecr_image" "api_image" {
+  repository_name = aws_ecr_repository.api.name
+  most_recent     = true
+}
+
+output "api_image" {
+  value = data.aws_ecr_image.api_image
 }
