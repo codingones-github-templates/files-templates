44983bd3859855f3add742d8a134150b18b3bdbb
Romain Cambonie <romain.cambonie@gmail.com>
2023-05-06 17:47:54 +0200

    test: adding cloudfront logs configuration with acl

--- terraform/service/client/s3.tf
+++ terraform/service/client/s3.tf
@@ -3,49 +3,82 @@ resource "aws_s3_bucket" "client" {
   tags   = local.tags
 }
 
-data "aws_iam_policy_document" "s3_bucket_policy" {
-  statement {
-    sid       = "GrantCloudFrontReadAccess"
-    effect    = "Allow"
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.client.arn}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.client.iam_arn]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.client.id
-  policy = data.aws_iam_policy_document.s3_bucket_policy.json
-}
-
-
-data "aws_iam_policy_document" "client_s3_policy" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.client.arn}/*"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.client.iam_arn]
-    }
-  }
-
-  statement {
-    actions   = ["s3:ListBucket"]
-    resources = [aws_s3_bucket.client.arn]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.client.iam_arn]
-    }
-  }
-}
-
 resource "aws_s3_bucket_policy" "client" {
   bucket = aws_s3_bucket.client.id
-  policy = data.aws_iam_policy_document.client_s3_policy.json
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject"
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.client.arn}/*"
+        ],
+        "Principal" : {
+          "AWS" : [
+            "${aws_cloudfront_origin_access_identity.client.iam_arn}"
+          ]
+        }
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.client.arn}"
+        ],
+        "Principal" : {
+          "AWS" : [
+            "${aws_cloudfront_origin_access_identity.client.iam_arn}"
+          ]
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "${var.project}-${var.service}-logs"
+  tags   = local.tags
+}
+
+resource "aws_s3_bucket_ownership_controls" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "logs" {
+  depends_on = [aws_s3_bucket_ownership_controls.logs]
+
+  bucket = aws_s3_bucket.logs.id
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket_policy" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : [
+          "s3:GetBucketAcl",
+          "s3:PutBucketAcl",
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ],
+        "Resource" : ["${aws_s3_bucket.logs.arn}/*"]
+      }
+    ]
+    }
+  )
 }
